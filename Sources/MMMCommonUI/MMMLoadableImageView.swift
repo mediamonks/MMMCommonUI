4429a173b5d7ab60dc5e196fc86a7ea4c12421d8
Aleh Dzenisiuk <aleh@mediamonks.com>
2020-11-16 22:09:46 +0100

    Import MMMLoadableImageView
    Added the test as well, but not sure how to integrate the snapshots. TBD.

---
+++ Sources/MMMCommonUI/MMMLoadableImageView.swift
@@ -0,0 +1,150 @@
+//
+// MMMCommonUI. Part of MMMTemple.
+// Copyright (C) 2016-2020 MediaMonks. All rights reserved.
+//
+
+import Foundation
+
+/// An image view that is able to work with loadable images directly.
+public final class MMMLoadableImageView: NonStoryboardableView {
+
+	/// Scaling mode that should be used in case external constraints applied to this view do not allow
+	/// it to have its natural size.
+	///
+	/// Note that there is no need in supporting all values of `UIView.ContentMode`.
+	/// It makes no sense to not scale images loaded from somewhere or to scale them disproportionally.
+	public enum Mode {
+		case fill
+		case fit
+	}
+
+	// Not inheriting from UIImageView, so it's easier to start clipping it at some point, add non-trivial background,
+	// render decoration or animate loading state.
+	// Using `MMMImageView` to solve troubles with alignment rects and take care of proportion constraints.
+	private let imageView = MMMImageView()
+
+	/// A view that can be used by outside views to align decoration views (e.g. shadows) with the bounds
+	/// of the actual image. Use this only for constraints, don't change borders, background, etc.
+	///
+	/// Note that in case of the `.fill` mode the bounds of this view might reside outside of the receiver's bounds.
+	public var alignmentView: UIView { imageView }
+
+	// No reason to open this for changes at any time.
+	private let placeholderImage: UIImage?
+
+	private var imageObserver: MMMLoadableObserver?
+
+	/// A loadable image this image view should display and track. Just set and forget.
+	/// (Well, think of the max size you would like this image view to have and set the corresponding constraints.)
+	public var image: MMMLoadableImage? {
+		didSet {
+			guard image !== oldValue else {
+				return
+			}
+			if let loadableImage = image {
+				imageObserver = MMMLoadableObserver(loadable: loadableImage) { [weak self] _ in
+					self?.update(animated: true)
+				}
+			} else {
+				imageObserver = nil
+			}
+			image?.syncIfNeeded()
+			update(animated: false)
+		}
+	}
+
+	// MARK: Init
+
+	// TODO: visually distinguish between 'loading' and 'failed to load' states, e.g. by using two placeholders or possibly using "shimmer"-kind animation.
+	// TODO: this is where effects like shadows and corners can be added as well.
+	public init(placeholderImage: UIImage? = nil, mode: Mode = .fit) {
+
+		self.placeholderImage = placeholderImage
+
+		super.init()
+
+		self.clipsToBounds = (mode == .fill)
+
+		addSubview(imageView)
+
+		do {
+
+			let views = [ "imageView": imageView ]
+
+			switch mode {
+			case .fill:
+				NSLayoutConstraint.activate(NSLayoutConstraint.constraints(
+					withVisualFormat: "H:|-(<=0,0@751)-[imageView]-(<=0,0@751)-|",
+					metrics: nil, views: views
+				))
+				NSLayoutConstraint.activate(NSLayoutConstraint.constraints(
+					withVisualFormat: "V:|-(<=0,0@751)-[imageView]-(<=0,0@751)-|",
+					metrics: nil, views: views
+				))
+			case .fit:
+				// As always, using 749, so we pin only if that's possible without deforming the image view.
+				NSLayoutConstraint.activate(NSLayoutConstraint.constraints(
+					withVisualFormat: "H:|-(>=0,0@749)-[imageView]-(>=0,0@749)-|",
+					metrics: nil, views: views
+				))
+				NSLayoutConstraint.activate(NSLayoutConstraint.constraints(
+					withVisualFormat: "V:|-(>=0,0@749)-[imageView]-(>=0,0@749)-|",
+					metrics: nil, views: views
+				))
+			}
+
+			// Regardless of the mode we need to make sure the image is centered.
+
+			NSLayoutConstraint.activate(NSLayoutConstraint(
+				item: imageView, attribute: .centerX,
+				relatedBy: .equal,
+				toItem: self, attribute: .centerX,
+				multiplier: 1, constant: 0,
+				priority: .defaultHigh - 2
+			))
+			NSLayoutConstraint.activate(NSLayoutConstraint(
+				item: imageView, attribute: .centerY,
+				relatedBy: .equal,
+				toItem: self, attribute: .centerY,
+				multiplier: 1, constant: 0,
+				priority: .defaultHigh - 2
+			))
+		}
+
+		update(animated: false)
+	}
+
+	private func update(animated: Bool) {
+
+		guard let loadableImage = image else {
+			imageView.image = placeholderImage
+			return
+		}
+
+		if loadableImage.isContentsAvailable {
+			updateImage(with: loadableImage.image, animated: animated)
+		} else {
+			assert(loadableImage.loadableState == .syncing || loadableImage.loadableState == .didFailToSync)
+			// TODO: currently we don't distinguish between loading and failed here, but would be better to so.
+			updateImage(with: placeholderImage)
+		}
+	}
+	
+	private func updateImage(with image: UIImage?, animated: Bool = false) {
+		guard animated else {
+			imageView.image = image
+			return
+		}
+		UIView.transition(
+			with: imageView,
+			duration: 0.25,
+			options: [.beginFromCurrentState, .transitionCrossDissolve],
+			animations: {
+				self.imageView.image = image
+			},
+			completion: { (_) in
+				self.imageView.image = image
+			}
+		)
+	}
+}
---
+++ Tests/MMMLoadableImageViewTestCase.swift
@@ -0,0 +1,157 @@
+//
+// MMMCommonUI. Part of MMMTemple.
+// Copyright (C) 2016-2020 MediaMonks. All rights reserved.
+//
+
+import MMMCommonUI
+import MMMTestCase
+
+class MMMLoadableImageViewTestCase: MMMTestCase {
+
+    private func stubImage(
+    	size: CGSize,
+    	backgroundColor: UIColor,
+    	borderColor: UIColor,
+    	borderWidth: CGFloat = 1
+	) -> UIImage {
+
+    	let bounds = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+
+    	UIGraphicsBeginImageContextWithOptions(bounds.size, false, 0)
+
+    	let c = UIGraphicsGetCurrentContext()!
+
+		let halfBorder = borderWidth / 2
+    	c.addRect(bounds.insetBy(dx: halfBorder, dy: halfBorder))
+
+		backgroundColor.setFill()
+		borderColor.setStroke()
+		c.setLineWidth(borderWidth)
+		c.drawPath(using: .fillStroke)
+
+    	let result = UIGraphicsGetImageFromCurrentImageContext()!
+    	UIGraphicsEndImageContext()
+
+    	return result
+    }
+
+    private lazy var landscapeImage = {
+		return stubImage(
+			size: CGSize(width: 40, height: 20),
+			backgroundColor: MMMDebugColor(1),
+			borderColor: MMMDebugColor(2)
+		)
+	}()
+
+	private lazy var portraitImage = {
+		return stubImage(
+			size: CGSize(width: 20, height: 40),
+			backgroundColor: MMMDebugColor(2),
+			borderColor: MMMDebugColor(3)
+		)
+    }()
+
+    public func testModes() {
+
+    	// In this test I am only interested in the correct handling of fit/fill modes,
+    	// and a placeholder should be enough for that.
+
+    	self.varyParameters([
+    		"placeholder": [
+				"landscape": landscapeImage,
+				"portrait": portraitImage
+    		],
+    		"mode": [
+    			"fit": .fit,
+    			"fill": .fill
+    		] as [String: MMMLoadableImageView.Mode]
+    	]) { (identifier, values) in
+
+			let view = MMMLoadableImageView(
+				placeholderImage: values["placeholder"] as! UIImage?,
+				mode: values["mode"] as! MMMLoadableImageView.Mode
+			)
+
+			// Want it to stay square to test the modes.
+			NSLayoutConstraint.activate(NSLayoutConstraint(
+				item: view, attribute: .width,
+				relatedBy: .equal,
+				toItem: view, attribute: .height,
+				multiplier: 1, constant: 0
+			))
+
+			self.verify(
+				view: view,
+				fits: [
+					// Let's see that it can stretch if the image is smaller than the space we have.
+					.size(width: 60, height: 60)
+				],
+				identifier: identifier,
+				backgroundColor: MMMDebugColor(0)
+			)
+		}
+    }
+
+    public func testBasics() {
+
+    	// This is where I want to check if the "loadable" part of it is working OK.
+
+		let image = MMMTestLoadableImage()
+		XCTAssert(image.loadableState == .idle)
+		XCTAssert(!image.hasObservers)
+
+		// Want to make sure the view is deallocated and removes its observer.
+		autoreleasepool {
+
+			let view = MMMLoadableImageView(
+				placeholderImage: stubImage(
+					size: CGSize(width: 40, height: 40),
+					backgroundColor: MMMDebugColor(0),
+					borderColor: MMMDebugColor(0)
+				),
+				mode: .fill
+			)
+
+			NSLayoutConstraint.activate(NSLayoutConstraint(
+				item: view, attribute: .width,
+				relatedBy: .lessThanOrEqual,
+				toItem: nil, attribute: .notAnAttribute,
+				multiplier: 1, constant: 20
+			))
+			NSLayoutConstraint.activate(NSLayoutConstraint(
+				item: view, attribute: .height,
+				relatedBy: .lessThanOrEqual,
+				toItem: nil, attribute: .notAnAttribute,
+				multiplier: 1, constant: 20
+			))
+
+			// No image is set yet, should see only the placeholder.
+			self.verify(view: view, fit: .natural, identifier: "000_initial")
+
+			view.image = image
+			XCTAssert(image.loadableState == .syncing, "Should begin refreshing the image")
+			XCTAssert(image.hasObservers, "Should have started observing it")
+
+			// Currently should display a placeholder while syncing. Might be a shimmer at some point.
+			self.verify(view: view, fit: .natural, identifier: "001_syncing")
+
+			// Should continue displaying a placeholder after a failure.
+			image.setDidFailToSyncWithError(nil)
+			self.verify(view: view, fit: .natural, identifier: "002_failed")
+
+			// Should display our image once we got it.
+			image.setDidSyncSuccessfullyWith(landscapeImage)
+			self.verify(view: view, fit: .natural, identifier: "003_loaded")
+
+			// Refreshing again extrernally should keep the loaded image.
+			image.setSyncing()
+			self.verify(view: view, fit: .natural, identifier: "004_syncing_again")
+
+			// And failing it again should keep the existing one.
+			image.setDidFailToSyncWithError(nil)
+			self.verify(view: view, fit: .natural, identifier: "005_failed_again")
+		}
+
+		XCTAssert(!image.hasObservers)
+    }
+}
