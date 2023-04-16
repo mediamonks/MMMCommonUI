ea25c68f0fb724597eacf3acebbed4cbb7b27bd5
Aleh Dzenisiuk <aleh@mediamonks.com>
2023-04-16 16:45:12 +0200

    fixup! Work around build issues when this is used as a dev pod

--- Sources/MMMCommonUIObjC/MMMCommonUI.h
+++ Sources/MMMCommonUIObjC/MMMCommonUI.h
@@ -4,3 +4,5 @@
 //
 
 // This file is needed because Swift compatibility headers always contain `#import <MMMCommonUI/MMMCommonUI.h>` line.
+// Let's re-use the auto-generated umbrella header instead of repeating all the imports here.
+#import "MMMCommonUI-umbrella.h"
